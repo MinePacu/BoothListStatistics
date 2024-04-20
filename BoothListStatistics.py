52ffe21af10921c196799ac3379af67783e9549e
Pacu <minepacu@gmail.com>
2024-04-20 12:25:42 +0900

    개선 사항 #3 반영을 위한 코드 추가
    - BoothListStatistics.py: #3 (업데이트 시간 추가)를 위한 로직 코드 추가

--- BoothListStatistics.py
+++ BoothListStatistics.py
@@ -4,6 +4,7 @@ from gspread.utils import ValueRenderOption, ValueInputOption
 import gspread.utils
 import gspread_formatting
 from gspread_formatting import Borders
+from datetime import datetime
 import time
 
 def clean_N_InList(GenreLists: list[list[str]]):
@@ -80,8 +81,10 @@ sheetNumber = 0
 statistics_spreadsheetId = "1qmIvmDX9GdS8yoMlIaVTGAwphpTU7xwfq-uLrubfDTk"
 statictics_sheetNumber = 0
 
+statictics_sheetUpdatetime_al = 'C3'
+
 # 통계 워크 시트에서 자료를 넣기 시작하는 지점 (1부터 시작)
-statictics_sheetStartIndex = 5
+statictics_sheetStartIndex = 6
 
 client_ = gspread.service_account()
 sh = client_.open_by_key(spreadsheetId)
@@ -119,6 +122,11 @@ grade_Index = 1					# 순위 인덱스
 duplicate_index = 1				# 장르 개수가 중복되는 경우, 1씩 증가하는 중복 인덱스
 index = 1						# 인덱스
 former_value = 0				# 이전 인덱스의 장르 개수 (개수가 같은지 아닌지를 비교하는데 사용됨)
+
+updatetime = datetime.now()
+sheet_genre.update_acell(f'{statictics_sheetUpdatetime_al}',
+					 f'{updatetime.year}. {updatetime.month}. {updatetime.day} {updatetime.hour}:{str(updatetime.minute).zfill(2)}:{str(updatetime.second).zfill(2)}')
+
 for key in sorted_result:
 	if index != 1:												# 인덱스가 1인 경우, 이전 데이터가 없으므로 인덱스가 1인 경우를 제외함
 		if sorted_result[key] == former_value:					# 이전 장르 개수와 동일하면, 중복 인덱스 1 증가
